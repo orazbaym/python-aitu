ce73432ad0c7168e200b12c47721603a502ece74
orazbaym <68743608+orazbaym@users.noreply.github.com>
2021-09-19 22:34:10 +0600

    first commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+a = ["a", "b", "c"]
+b = a[:]
+
+for i in range(0, 3):   
+   for j in range(0, 3):
+        for k in range(0, 3):
+            if(a[i] != a[j] and a[i] != a[k] and a[j] !=a[k]):
+                print(a[i] + a[j] + a[k])
+  
+   
+
